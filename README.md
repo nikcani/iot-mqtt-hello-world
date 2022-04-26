2c6b499a70123b4eb802febe2b1a98cf1229bd03
Niklas Canisius <niklascanisius@me.com>
2022-04-26 22:13:16 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,20 @@
+# Broker Setup
+- Ubuntu 20.04.4
+- https://mosquitto.org/download/#ubuntu
+- /etc/mosquitto/conf.d/
+    ```bash
+    listener 1883
+    allow_anonymous true
+    ```
+  further reading
+  - https://mosquitto.org/man/mosquitto-conf-5.html
+  - https://mosquitto.org/documentation/authentication-methods
+- sudo service mosquitto restart
+
+# Node-RED Setup
+- Install docker: https://docs.docker.com/get-docker/
+- ```bash
+  docker run -it -p 1880:1880 -v node_red_data:/data --name mynodered nodered/node-red
+  ```
+  https://nodered.org/docs/getting-started/docker
+- Visit http://localhost:1880
---
+++ src/main.cpp
@@ -0,0 +1,112 @@
+#include <Arduino.h>
+#include <arduino_secrets.h>
+#include <Wifi.h>
+#include <PubSubClient.h>
+
+#define PIN_NO_BUTTON 26
+#define PIN_NO_LED_RED 14
+#define PIN_NO_LED_GREEN 22
+#define PIN_NO_BUZZER 33
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+volatile bool buttonState;
+
+void buttonClicked() {
+    buttonState = digitalRead(PIN_NO_BUTTON);
+}
+
+void writeForOneSecond(int pin) {
+    digitalWrite(pin, 1);
+    delay(1000);
+    digitalWrite(pin, 0);
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
+    }
+    Serial.println();
+
+    if (strcmp(topic, "led") == 0) {
+        writeForOneSecond(PIN_NO_LED_RED);
+        writeForOneSecond(PIN_NO_LED_GREEN);
+    } else if (strcmp(topic, "led_red") == 0) {
+        writeForOneSecond(PIN_NO_LED_RED);
+    } else if (strcmp(topic, "led_green") == 0) {
+        writeForOneSecond(PIN_NO_LED_GREEN);
+    } else if (strcmp(topic, "buzzer") == 0) {
+        writeForOneSecond(PIN_NO_BUZZER);
+    }
+}
+
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        if (client.connect("arduinoClient")) {
+            Serial.println("connected");
+            client.publish("foo", "bar");
+            client.subscribe("led");
+            client.subscribe("led_red");
+            client.subscribe("led_green");
+            client.subscribe("buzzer");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+static void wifi_init(void) {
+    Serial.begin(115200);
+    WiFi.begin(SECRET_WIFI_SSID, SECRET_WIFI_PASS);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
+    Serial.println(WiFi.gatewayIP());
+    Serial.println(WiFi.localIP());
+}
+
+static void mqtt_init(void) {
+    client.setServer(SECRET_MQTT_SERVER, 1883);
+    client.setCallback(callback);
+}
+
+void setup() {
+    pinMode(PIN_NO_LED_RED, OUTPUT);
+    digitalWrite(PIN_NO_LED_RED, LOW);
+
+    pinMode(PIN_NO_LED_GREEN, OUTPUT);
+    digitalWrite(PIN_NO_LED_GREEN, LOW);
+
+    pinMode(PIN_NO_BUZZER, OUTPUT);
+    digitalWrite(PIN_NO_BUZZER, LOW);
+
+    pinMode(PIN_NO_BUTTON, INPUT);
+    attachInterrupt(PIN_NO_BUTTON, buttonClicked, CHANGE);
+
+    wifi_init();
+
+    mqtt_init();
+}
+
+void loop() {
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+    if (buttonState) {
+        buttonState = false;
+        client.publish("button", "pressed");
+    }
+}
